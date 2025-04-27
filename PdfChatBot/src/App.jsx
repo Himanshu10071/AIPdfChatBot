0706d687aaada4766d956e282fc21afb225866e1
Himanshu Chauhan <himaanschouhan16@gmail.com>
2025-04-28 00:42:13 +0530

    Project Started

---
+++ PdfChatBot/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <input type="file" />
+    </>
+  )
+}
+
+export default App
